6c00be19597b4a100fa1701374c9680943a4c76f
afterresultsolutions <afterresult.solutions@gmail.com>
2025-05-30 22:29:26 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,14 +6,14 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'AfterResult Solutions',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://afterresult.solutions' }],
 			sidebar: [
 				{
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Learn More', slug: 'guides/example' },
 					],
 				},
 				{
